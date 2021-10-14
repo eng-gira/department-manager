7801ac59d77a565f4950695c73a168998ec8df68
A. Osama <Altimos@desktop.com>
2021-10-14 23:02:08 +0200

    more stylish IoT

--- resources/views/iot/index.blade.php
+++ resources/views/iot/index.blade.php
@@ -1,21 +1,27 @@
-
-@if(is_array($data))
-    <table class="table">
-        <tr>
-            <th>Key</th>
-            <th>Value</th>
-        </tr>
-        @foreach($data as $k=>$v)
-            <tr>
-                <td>
-                    {{$k}}
-                </td>
-                <td>
-                    {{$v}}
-                </td>
-            </tr>
-        @endforeach
-    </table>
-@else
-    {{$data}}
-@endif
+<x-app-layout>
+    <x-slot name="header">
+        <h4>IoT</h4>
+    </x-slot>
+    <div class="container">
+        @if(is_array($data))
+            <table class="table">
+                <tr>
+                    <th>Key</th>
+                    <th>Value</th>
+                </tr>
+                @foreach($data as $k=>$v)
+                    <tr>
+                        <td>
+                            {{$k}}
+                        </td>
+                        <td>
+                            {{$v}}
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        @else
+            {{$data}}
+        @endif
+    </div>
+</x-app-layout>
